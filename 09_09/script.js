4994dabf46f6be3f3e5a8c178687648306af1223
Tauno Erik <sedumacre@gmail.com>
2025-11-12 19:52:40 +0200

    update valu

--- 09_09/script.js
+++ 09_09/script.js
@@ -5,17 +5,28 @@
 // when button is clicked, the event listener should update the contents inside
 // <span class="value"> for the relevant value.
 
+
 const all_forms = document.querySelectorAll("form");
+
 
 all_forms.forEach((form) => {
   form.addEventListener("submit", (event) => {
     // disable page reload
     event.preventDefault();
-    const target_name = event.target.getAttribute("data-target");
+
+    // Get the attribute from the button that submitted the form
+    const target_name = event.submitter.getAttribute("data-target");
     console.log(target_name);
+
     const input_el = document.getElementById(`${target_name}-input`);
     const target_container = document.getElementById(target_name);
     const target_el = target_container?.querySelector(".value");
     target_el.textContent = input_el.value;
+
+    if (target_el && input_el) {
+      target_el.textContent = input_el.value;
+      // Optional: Clear the input after submitting
+      input_el.value = ""; 
+    }
   });
 });
